d83de02b1b6523938a89520378b94e2ccdfa3a2c
Paul Kulchenko <paul@kulchenko.com>
2012-09-03 18:02:54 -0700

    Updated README and rockspec for v0.49

--- src/mobdebug.lua
+++ src/mobdebug.lua
@@ -1,12 +1,12 @@
 --
--- MobDebug 0.489
+-- MobDebug 0.49
 -- Copyright 2011-12 Paul Kulchenko
 -- Based on RemDebug 1.0 Copyright Kepler Project 2005
 --
 
 local mobdebug = {
   _NAME = "mobdebug",
-  _VERSION = 0.489,
+  _VERSION = 0.49,
   _COPYRIGHT = "Paul Kulchenko",
   _DESCRIPTION = "Mobile Remote Debugger for the Lua programming language",
   port = 8171
