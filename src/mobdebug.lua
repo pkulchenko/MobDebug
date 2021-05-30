00a24e4dc16e0de23837149bafdd9345148e9054
Catalin Marincia <catalinmarincia@gmail.com>
2021-05-30 21:19:19 +0300

    Add luaspeck and prepare for new version

--- src/mobdebug.lua
+++ src/mobdebug.lua
@@ -19,7 +19,7 @@ end)("os")
 
 local mobdebug = {
   _NAME = "mobdebug",
-  _VERSION = "0.709",
+  _VERSION = "0.801",
   _COPYRIGHT = "Paul Kulchenko",
   _DESCRIPTION = "Mobile Remote Debugger for the Lua programming language",
   port = os and os.getenv and tonumber((os.getenv("MOBDEBUG_PORT"))) or 8172,
