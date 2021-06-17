11d8f83baecac9c39d7868d8fbc74297ca2efbea
Paul Kulchenko <paul@kulchenko.com>
2021-06-16 22:56:46 -0700

    Removed debug.traceback patch to improve compatibility (closes #54).

--- src/mobdebug.lua
+++ src/mobdebug.lua
@@ -19,7 +19,7 @@ end)("os")
 
 local mobdebug = {
   _NAME = "mobdebug",
-  _VERSION = "0.80",
+  _VERSION = "0.801",
   _COPYRIGHT = "Paul Kulchenko",
   _DESCRIPTION = "Mobile Remote Debugger for the Lua programming language",
   port = os and os.getenv and tonumber((os.getenv("MOBDEBUG_PORT"))) or 8172,
@@ -1068,33 +1068,6 @@ local function start(controller_host, controller_port)
     -- start from 16th frame, which is sufficiently large for this check.
     stack_level = stack_depth(16)
 
-    -- provide our own traceback function to report errors remotely
-    -- but only under Lua 5.1/LuaJIT as it's not called under Lua 5.2+
-    -- (http://lua-users.org/lists/lua-l/2016-05/msg00297.html)
-    local function f() return function()end end
-    if f() ~= f() then -- Lua 5.1 or LuaJIT
-      local dtraceback = debug.traceback
-      debug.traceback = function (...)
-        if select('#', ...) >= 1 then
-          local thr, err, lvl = ...
-          if type(thr) ~= 'thread' then err, lvl = thr, err end
-          local trace = dtraceback(err, (lvl or 1)+1)
-          if genv.print == iobase.print then -- no remote redirect
-            return trace
-          else
-            genv.print(trace) -- report the error remotely
-            return -- don't report locally to avoid double reporting
-          end
-        end
-        -- direct call to debug.traceback: return the original.
-        -- debug.traceback(nil, level) doesn't work in Lua 5.1
-        -- (http://lua-users.org/lists/lua-l/2011-06/msg00574.html), so
-        -- simply remove first frame from the stack trace
-        local tb = dtraceback("", 2) -- skip debugger frames
-        -- if the string is returned, then remove the first new line as it's not needed
-        return type(tb) == "string" and tb:gsub("^\n","") or tb
-      end
-    end
     coro_debugger = corocreate(debugger_loop)
     debug.sethook(debug_hook, HOOKMASK)
     seen_hook = nil -- reset in case the last start() call was refused
@@ -1158,7 +1131,7 @@ local function controller(controller_host, controller_port, scratchpad)
           -- check if the debugging is done (coro_debugger is nil)
           if not coro_debugger then break end
           -- resume once more to clear the response the debugger wants to send
-          -- need to use capture_vars(0) to capture only two (default) level,
+          -- need to use capture_vars(0) to capture only two (default) levels,
           -- as even though there is controller() call, because of the tail call,
           -- the caller may not exist for it;
           -- This is not entirely safe as the user may see the local
